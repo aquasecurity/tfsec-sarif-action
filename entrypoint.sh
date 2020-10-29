86ebd10c6beff5fd7fc68b5101dc18e2477aac40
Owen Rumney <owen@owenrumney.co.uk>
2020-10-29 14:19:51 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,3 @@ tfsec --format=sarif "${INPUT_WORKING_DIRECTORY}" > ${INPUT_SARIF_FILE}
 tfsec_return="${PIPESTATUS[0]}" exit_code=$?
 
 echo ::set-output name=tfsec-return-code::"${tfsec_return}"
-
-# echo "Input branch is ${INPUT_BRANCH}"
-
-# sh -c "git remote add origin ${INPUT_BRANCH} || git fetch --unshallow origin"
-
-# sh -c "git config --global user.name '${GITHUB_ACTOR}' \
-#       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
-#       && git add .sarif/tfsec.sarif && git commit -m 'Updating tfsec.sarif file' --allow-empty \
-#       && git push -u origin HEAD:${INPUT_BRANCH} --force"
-
