1bb5f9f1b26375656ac29a6fe2e6cd3e44dc913f
Alain Hélaïli <alain@carbonhub.io>
2021-06-08 14:10:45 +0200

    adding a tfvars_file option

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,17 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
+if [ -n "${INPUT_TFVARS_FILE}" ]; then
+   echo "::debug::Using tfvars file ${INPUT_TFVARS_FILE}"
+   TFVARS_OPTION="--tfvars-file ${INPUT_TFVARS_FILE}"
+fi
+
 echo {} > ${INPUT_SARIF_FILE}
 
-tfsec --format=sarif "${INPUT_WORKING_DIRECTORY}" > ${INPUT_SARIF_FILE}
+tfsec --format=sarif "${INPUT_WORKING_DIRECTORY}" ${TFVARS_OPTION} > ${INPUT_SARIF_FILE}
 
 tfsec_return="${PIPESTATUS[0]}" exit_code=$?
 
 echo ::set-output name=tfsec-return-code::"${tfsec_return}"
+
+
