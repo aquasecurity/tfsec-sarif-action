f3587c53d3fe9bcc3de986cd814ffa3a7a16f7fa
Owen Rumney <owen.rumney@aquasec.com>
2021-07-08 11:55:45 +0100

    Support specifying the tfsec version
    - move the download of tfsec to the entrypoint
    - add to the action file the input parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,24 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-if [ -n "${INPUT_TFVARS_FILE}" ]; then
-   echo "::debug::Using tfvars file ${INPUT_TFVARS_FILE}"
-   TFVARS_OPTION="--tfvars-file ${INPUT_TFVARS_FILE}"
+VERSION="latest"
+if [ "$INPUT_TFSEC_VERSION" != "latest" ]; then
+  VERSION="tags/${INPUT_TFSEC_VERSION}"
 fi
 
-echo {} > ${INPUT_SARIF_FILE}
+# Download the required tfsec version
+wget -O - -q "$(wget -q https://api.github.com/repos/tfsec/tfsec/releases/${VERSION} -O - | grep -o -E "https://.+?tfsec-linux-amd64" | head -n1)" >tfsec
+install tfsec /usr/local/bin/tfsec
 
-tfsec --format=sarif "${INPUT_WORKING_DIRECTORY}" ${TFVARS_OPTION} > ${INPUT_SARIF_FILE}
+if [ -n "${INPUT_TFVARS_FILE}" ]; then
+  echo "::debug::Using tfvars file ${INPUT_TFVARS_FILE}"
+  TFVARS_OPTION="--tfvars-file ${INPUT_TFVARS_FILE}"
+fi
+
+echo {} >${INPUT_SARIF_FILE}
+
+tfsec --format=sarif "${INPUT_WORKING_DIRECTORY}" ${TFVARS_OPTION} >${INPUT_SARIF_FILE}
 
 tfsec_return="${PIPESTATUS[0]}" exit_code=$?
 
 echo ::set-output name=tfsec-return-code::"${tfsec_return}"
-
-
