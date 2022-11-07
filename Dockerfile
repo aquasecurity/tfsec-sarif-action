aquasecurity/tfsec-sarif-action

commit 8019886f8d2580cb3b80fb3490d2167ce8a116e2

Forcing wget to use IPv4 (#37)
* Update entrypoint.sh

* Update Dockerfile

* Update entrypoint.sh

* Update Dockerfile

* Update Dockerfile

* Update entrypoint.sh

* Update Dockerfile

* Update Dockerfile

Dockerfile
@@ -1,9 +1,11 @@
-FROM alpine:3.15.2
+FROM alpine:3.16.2
 
-RUN apk --no-cache --update add bash
+RUN apk --no-cache --update add bash wget
 
 SHELL ["/bin/bash", "-eo", "pipefail", "-c"]
 
 COPY entrypoint.sh /entrypoint.sh
+ADD https://github.com/aquasecurity/tfsec/releases/download/v1.28.1/tfsec-linux-amd64 .
+RUN install tfsec-linux-amd64 /usr/local/bin/tfsec
 
 ENTRYPOINT ["/entrypoint.sh"]

entrypoint.sh
@@ -15,14 +15,14 @@ if [[ -n "${INPUT_TFSEC_VERSION}" && "$INPUT_TFSEC_VERSION" != "latest" ]]; then
   TFSEC_VERSION="tags/${INPUT_TFSEC_VERSION}"
 fi
 
-# Pull https://api.github.com/repos/aquasecurity/tfsec/releases for the full list of releases. NOTE no trailing slash
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec-linux-amd64" | head -n1)" > tfsec-linux-amd64
-wget -O - -q "$(wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec_checksums.txt" | head -n1)" > tfsec.checksums
+# # Pull https://api.github.com/repos/aquasecurity/tfsec/releases for the full list of releases. NOTE no trailing slash
+# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec-linux-amd64" | head -n1)" > tfsec-linux-amd64
+# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec_checksums.txt" | head -n1)" > tfsec.checksums
 
-# pipe out the checksum and validate
-grep tfsec-linux-amd64 tfsec.checksums > tfsec-linux-amd64.checksum
-sha256sum -c tfsec-linux-amd64.checksum
-install tfsec-linux-amd64 /usr/local/bin/tfsec
+# # pipe out the checksum and validate
+# grep tfsec-linux-amd64 tfsec.checksums > tfsec-linux-amd64.checksum
+# sha256sum -c tfsec-linux-amd64.checksum
+# install tfsec-linux-amd64 /usr/local/bin/tfsec
 
 # if input vars file then add to arguments
 if [ -n "${INPUT_TFVARS_FILE}" ]; then

commit d20328e699ee4ad0114f5422acf0dcf78bfe0faa

fix: Use the INPUT_TFSEC_VERSION instead of hard-coding the version
Fix an issue introduced by https://github.com/aquasecurity/tfsec-sarif-action/pull/37

Dockerfile
@@ -5,7 +5,5 @@ RUN apk --no-cache --update add bash wget git mercurial
 SHELL ["/bin/bash", "-eo", "pipefail", "-c"]
 
 COPY entrypoint.sh /entrypoint.sh
-ADD https://github.com/aquasecurity/tfsec/releases/download/v1.28.1/tfsec-linux-amd64 .
-RUN install tfsec-linux-amd64 /usr/local/bin/tfsec
 
 ENTRYPOINT ["/entrypoint.sh"]

entrypoint.sh
@@ -15,14 +15,15 @@ if [[ -n "${INPUT_TFSEC_VERSION}" && "$INPUT_TFSEC_VERSION" != "latest" ]]; then
   TFSEC_VERSION="tags/${INPUT_TFSEC_VERSION}"
 fi
 
-# # Pull https://api.github.com/repos/aquasecurity/tfsec/releases for the full list of releases. NOTE no trailing slash
-# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec-linux-amd64" | head -n1)" > tfsec-linux-amd64
-# wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec_checksums.txt" | head -n1)" > tfsec.checksums
+# Pull https://api.github.com/repos/aquasecurity/tfsec/releases for the full list of releases. NOTE no trailing slash
+wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec-linux-amd64" | head -n1)" > tfsec-linux-amd64
+wget --inet4-only -O - -q "$(wget --inet4-only -q https://api.github.com/repos/aquasecurity/tfsec/releases/${TFSEC_VERSION} -O - | grep -m 1 -o -E "https://.+?tfsec_checksums.txt" | head -n1)" > tfsec.checksums
 
-# # pipe out the checksum and validate
-# grep tfsec-linux-amd64 tfsec.checksums > tfsec-linux-amd64.checksum
-# sha256sum -c tfsec-linux-amd64.checksum
-# install tfsec-linux-amd64 /usr/local/bin/tfsec
+# pipe out the checksum and validate
+grep tfsec-linux-amd64 tfsec.checksums > tfsec-linux-amd64.checksum
+sha256sum -c tfsec-linux-amd64.checksum
+install tfsec-linux-amd64 /usr/local/bin/tfsec
+tfsec --version
 
 # if input vars file then add to arguments
 if [ -n "${INPUT_TFVARS_FILE}" ]; then